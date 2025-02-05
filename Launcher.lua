89f13067639100e18cdaa74fe916d97b2970d2b0
Tempest <59133811+10tempest01@users.noreply.github.com>
2025-02-05 08:36:57 +0300

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -3,5 +3,5 @@ local supportedGames = {
 }
 
 if table.find(supportedGames, tostring(game.GameId)) then
-    loadstring()()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/10tempest01/tempest-hub/refs/heads/main/Games/" .. game.GameId .. ".lua"))()
 end
